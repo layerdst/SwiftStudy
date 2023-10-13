cb5dff5102101c4ad68e2c2dcf46bcca37e989cf
layerdst <bccfcc@hanmail.net>
2023-10-13 22:25:49 +0900

    loginProject

---
+++ appMake/LoginProject/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  LoginProject
+//
+//  Created by 김재영 on 2023/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let emailTextFieldView = UIView()
+//
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        makeIUI()
+        
+        
+    }
+    
+    func makeIUI(){
+        emailTextFieldView.backgroundColor = UIColor.darkGray
+        view.addSubview(emailTextFieldView)
+        
+        emailTextFieldView.layer.cornerRadius = 5
+        emailTextFieldView.layer.masksToBounds = true
+                
+        // 오토레이아웃 기능을 끄는것, 반드시 설정해야지만 수동으로 레이아웃을 설정이 가능함
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // 텍스트뷰 화면 배치
+        // leadingAnchor : 화면 좌측에서
+        // trailingAnchor : 화면 우측
+        // topAnchor : 화면 상단
+        // heightAnchor : 뷰 높이
+        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        
+        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+    }
+
+
+}
+
