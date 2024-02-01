140ab8f2bd92b22d3a6c7db09a8478b38a0d15a3
layerdst <bccfcc@hanmail.net>
2024-02-02 06:23:00 +0900

    story project assignment

---
+++ appMake/Story/Destini-iOS13/Model/StoryBrain.swift
@@ -0,0 +1,39 @@
+//
+//  StoryBrain.swift
+//  Destini-iOS13
+//
+//  Created by Angela Yu on 08/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct StoryBrain {
+    
+    let story = [
+        Story(title: "You see a fork in the road ", choice1: "Take a left", choice2: "Take a right"),
+        Story(title: "You see a tiger", choice1: "Shout a help", choice2:  "Play dead"),
+        Story(title: "You find a treasure chest", choice1: "Opne it", choice2: "Check fo traps")
+    ]
+    
+    var storyNumber = 0
+    
+    mutating func nextStory(choice : String){
+        
+        switch (storyNumber, choice) {
+        case (0, "Take a left") :
+            storyNumber = 1
+            print("case0")
+        case (0, "Take a right") :
+            print("case2")
+            storyNumber = 2
+        default :
+            storyNumber = 0
+        }
+    }
+    
+    
+    func displayStory () -> Story {
+        return story[storyNumber]
+    }
+}
